a06a472a6931507fcc52e14087baf45a691e63ca
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-25 23:54:21 -0800

    combining-prop-drilling-and-context

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Comment from "./Comment";
+import UserInfoContext from "../context/UserInfoContext";
+
 export default function Post() {
+    const { username, isAdmin } = React.useContext(UserInfoContext);
+
     return (<div>
-        <h2>Post Title</h2>
+        <h2>Post Title {isAdmin && <button>Delete</button>}</h2>
         <p> Post description</p>
-        <Comment />
+        <Comment username={username} isAdmin={isAdmin} />
     </div>)
 }
